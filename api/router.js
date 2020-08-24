ef0ae4db69fc7a2a142ec30453a4cb5beb0440f1
UMESHRAUT <rautumesh34@gmail.com>
2020-08-24 18:29:18 +0530

    make it better

--- api/router.js
+++ api/router.js
@@ -8,34 +8,34 @@ router.post("/add-issues",(req,res)=>{
     console.log(req.body);
 
     const issues=new Issues(req.body)
-    console.log(issues);
+    // console.log(issues);
     issues.save().then(saved=>{res.json(saved);console.log(saved);}).catch(err=>res.json(err))
 })
 
 router.get("/list-issues",async(req,res)=>{
+    
     const page=req.query.page;
 
-    const startIndex=(page-1)*5;
+    const startIndex=(page-1)*10;
     const endIndex=page*10;
     const results={};
-
-    if(endIndex<Issues.length){
-        results.next={
-            page:page+1,
-        }    
+    
+    const count =await Issues.count({})
+    console.log(count);
+    if(endIndex<count){
+        results.next=Math.ceil(parseFloat(count)/10)
     }
-
+    results.current=parseInt(page);
+    console.log(page);
     if(startIndex>0){
-        results.previous={
-            page:page-1,
-        }
+        results.previous=page-1
     }
 try {
     results.result=await Issues.find().limit(10).skip(startIndex).exec()    
 } catch (error) {
     res.status(500).json({status:failed,Message:error.message})
 }
-console.log(results);
+// console.log(results);
    res.json(results)
 })
 
