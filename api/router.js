cec780f9259b8018527d88460a71b55c19412557
UMESHRAUT <rautumesh34@gmail.com>
2020-09-08 11:14:12 +0530

    updates

--- api/router.js
+++ api/router.js
@@ -16,6 +16,7 @@ router.patch("/api/update-issues/:id",async(req,res)=>{
     try {
         const status=await Issues.findByIdAndUpdate(id,{state:"close"});
         console.log(status.state);
+        status.save();
         // Issues.findOne({_id:id})
         res.json({
             Status: status,
